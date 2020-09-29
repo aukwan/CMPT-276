3dcc2deb34b2685d5ba98125d709e3db3d352d8e
aukwan <austinkwan32@gmail.com>
2020-09-29 15:11:34 -0700

    make activity screens

---
+++ app/src/main/java/ca/sfu/cmpt276/cameradepthoffieldapp/AddLens.java
@@ -0,0 +1,14 @@
+package ca.sfu.cmpt276.cameradepthoffieldapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddLens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_lens);
+    }
+}
---
+++ app/src/main/java/ca/sfu/cmpt276/cameradepthoffieldapp/CalculateDOF.java
@@ -0,0 +1,14 @@
+package ca.sfu.cmpt276.cameradepthoffieldapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculateDOF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_d_o_f);
+    }
+}
